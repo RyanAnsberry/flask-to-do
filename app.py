4c03059fa75e16d432ac069c551d5dabcca4becd
Ryan Ansberry <ansman92@gmail.com>
2021-02-15 14:23:18 -0800

    DB created

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class ToDo(db.model):
+class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
